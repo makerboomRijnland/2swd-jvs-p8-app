7a162e94a18d1f9caccf81b8b8e3ceb5caf3c5bb
makerboomRijnland <makerboom@mborijnland.nl>
2024-05-07 09:31:20 +0200

    User backend class

---
+++ app/models/user.js
@@ -0,0 +1,45 @@
+import {database} from '../database.js';
+import {Model, DataTypes} from 'sequelize';
+
+// export const User = sequelize.define('user', {
+//     id: {
+//         type: DataTypes.INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true
+//     },
+//     username: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//     },
+//     password: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//     }
+// });
+
+export class User extends Model {}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    passwordHash: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+},
+{
+  // Other model options go here
+  sequelize: database, // We need to pass the connection instance
+  modelName: 'User', // We need to choose the model name
+});
