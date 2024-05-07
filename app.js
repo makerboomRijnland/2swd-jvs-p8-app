281f205e895b8b609bb041245cdcb5625532383e
makerboomRijnland <makerboom@mborijnland.nl>
2024-05-07 08:52:36 +0200

    Setup sequelize and sqlite3

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-import express, { json } from 'express';
-import api from './routes/api.js';
+import express, { json } from "express";
+import apiRouter from "./routes/api.js";
+import userRouter from "./routes/user.js";
+import session from "express-session";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 app.use(json());
-app.use(express.static('static'));
+
+app.use(express.static("static"));
+
+app.use(
+    session({
+        secret: "keyboard cat",
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: true },
+    })
+);
+
+app.use("/api", apiRouter);
+app.use("/user", userRouter);
 
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
 });
-
-app.use('/api', api);
